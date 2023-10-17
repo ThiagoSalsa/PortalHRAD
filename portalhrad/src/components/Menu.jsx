25deba4b41cd27ad868541d3e632d873b4d267d2
Thiago Luis Trigueiro <thiagoluis@unipam.edu.br>
2023-10-17 02:22:39 -0300

    Criação do componente Menu
    Criação do componente Menu

---
+++ portalhrad/src/components/Menu.jsx
@@ -0,0 +1,18 @@
+export default function Menu(){
+    return (
+        <header className="header">
+            <h1>LOGO</h1>
+            
+            <ul>
+                <li><a href="">Informativos</a></li>
+                <li><a href="">Intranet</a></li>
+                <li><a href="">ExpressoMG</a></li>
+                <li><a href="">Agendas/Cirurgias</a></li>
+                <li><a href="">SIGH</a></li>
+                <li><a href="">Escalas</a></li>
+                <li><a href="">Suporte TI</a></li>
+            </ul>
+        </header>
+    )
+}
+
